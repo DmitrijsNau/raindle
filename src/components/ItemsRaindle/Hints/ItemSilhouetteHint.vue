c50f86409a7e3ac8f4eca7f7ea2b3977a9a01c7c
Dmitrijs Naudzuns <dmitrijs@buffalo.edu>
2024-05-07 19:11:01 -0400

    firebase rules and image styling

--- src/components/ItemsRaindle/Hints/ItemSilhouetteHint.vue
+++ src/components/ItemsRaindle/Hints/ItemSilhouetteHint.vue
@@ -1,8 +1,8 @@
 <template>
   <q-btn flat bordered square label="Item Silhouette Hint" @click="showSil = true" class="thick-border bg-blue-grey-14 q-mx-md"/>
   <q-dialog class="q-mx-md" v-model="showSil">
-    <q-card class="thick-border bg-bluegrey-14">
-        <q-img :src="todaysItemAnswer.itemSil" style="width: 80px; height: auto;"/>
+    <q-card class="thick-border bg-bluegrey-14 no-border-radius">
+        <q-img :src="todaysItemAnswer.itemSil" class="bg-blue-grey-14" style="width: 80px; height: auto;"/>
     </q-card>
   </q-dialog>
 </template>
@@ -16,3 +16,8 @@ defineProps({
   },
 });
 </script>
+<style scoped>
+.no-border-radius {
+  border-radius: 0 !important;
+}
+</style>
